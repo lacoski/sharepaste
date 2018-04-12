35103d162f52a734aeee7c7c0523b0255fd03943
Nguyen Hoai Nam <namptit307@gmail.com>
2018-04-13 00:04:21 +0700

    Update some syntaxes to follow the structure of Python
    This commit is to update some syntaxes.

--- tool-client/linux/pmtool.py
+++ tool-client/linux/pmtool.py
@@ -9,40 +9,41 @@ PART_TO_API = "/userzone/create_tool_guest/"
 # print("This is the name of the script: ", sys.argv[2])
 # print("Number of arguments: ", len(sys.argv))
 # print("The arguments are: " , str(sys.argv))
-if sys.stdin.isatty():
-    print('Fail - Content null')
-    print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
-    sys.exit(0)
-elif len(sys.argv) < 2:
-    print('Fail - argv not found')
-    print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
-    sys.exit(0)
-elif not sys.argv[1]:
-    print('Fail - null URL')
-    print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
-    sys.exit(0)
-elif not sys.argv[2]:
-    print('Fail - null Port')
-    print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
-    sys.exit(0)
 
-URL = "http://" + sys.argv[1] + ":" + sys.argv[2] + PART_TO_API
+def main():
+    if sys.stdin.isatty():
+        print('Fail - Content null')
+        print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
+        sys.exit(0)
+    elif len(sys.argv) < 2:
+        print('Fail - argv not found')
+        print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
+        sys.exit(0)
+    elif not sys.argv[1]:
+        print('Fail - null URL')
+        print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
+        sys.exit(0)
+    elif not sys.argv[2]:
+        print('Fail - null Port')
+        print('Ex: cat `<file>` | python3 pmtool.py `<URL>` `<PORT>`')
+        sys.exit(0)
 
-#print(URL)
-content=''
-for line in sys.stdin:        
-    content += line
+    URL = "http://{0}:{1}{2}".format(sys.argv[1], sys.argv[2], PART_TO_API])
 
-data = {
-        'paste_name': 'Up from PMtool',
-        'content_paste':content,        
-        'type_content_paste':'BASH',
-}
+    content=''
+    for line in sys.stdin:        
+        content += line
 
-r = requests.post(URL, data = data)
- 
-pastebin_url = r.text
+    data = {
+            'paste_name': 'Up from PMtool',
+            'content_paste':content,        
+            'type_content_paste':'BASH',
+            }
 
-print("Pastebin Short link is: %s"%pastebin_url)
+    r = requests.post(URL, data = data)
+    pasteme_url = r.text
+    print("Pastebin Short link is: {0}".format(pasteme_url))
+    # sudo cp pmtool.py /usr/local/bin/pmtool
 
-# sudo cp pmtool.py /usr/local/bin/pmtool
+if __name__ == "__main__":
+    main()
